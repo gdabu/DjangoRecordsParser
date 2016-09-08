2f78343b3451c228a2ca267456dc966094e12dbf
Geoff Dabu <geoffdabu@gmail.com>
2016-09-08 00:55:06 -0700

    functionally complete

--- renderer/views.py
+++ renderer/views.py
@@ -24,8 +24,21 @@ def period(request):
 	return HttpResponse(json.dumps(records))
 
 def poll(request):
-	records = mfreader.get_all_records()	
-	return HttpResponse(json.dumps(records))
+	periodType = request.GET.get('periodType');
+	periodVal = request.GET.get('periodVal');
+
+	if(periodType=="hours"):
+		records = mfreader.get_last_x_hours(periodVal)
+	elif(periodType=="days"):
+		records = mfreader.get_last_x_days(periodVal)
+	elif(periodType=="weeks"):
+		records = mfreader.get_last_x_weeks(periodVal)
+	else:
+		records = mfreader.get_all_records()	
+
+	
+	return HttpResponse(len(records))
+	
 
 def index(request):
 
